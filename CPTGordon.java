48f17374dad1757b695fb750ef22547393e4e56f
GrD0N <64982894+GrD0N@users.noreply.github.com>
2023-05-29 14:28:34 -0400

    Added Image

--- CPTGordon.java
+++ CPTGordon.java
@@ -1,15 +1,24 @@
 import arc.*;
 import java.awt.image.BufferedImage;
 import java.awt.Color;
+import java.awt.Font;
 
 public class CPTGordon{
 	public static void main(String [] args){
 		Console con = new Console("CPTGordon", 1280, 720);
-		BufferedImage imgStartScreen = con.loadImage("Connect4.jpg");
 		
-		con.drawImage(imgStartScreen,0,0);
-		con.drawString("(P) --> Play Game", 950, 200);
-		con.drawString("(H) --> View High Scores", 950, 250);
-		con.drawString("(R) --> View Replay", 950, 300);
+		int intKey = 0;
+		
+		BufferedImage imgStartScreen = con.loadImage("Connect4.jpg");
+		con.drawImage(imgStartScreen, 0, 0);
+		
+		con.drawString("(P) --> Play Game", 950, 350);
+		con.drawString("(H) --> View High Scores", 950, 400);
+		con.drawString("(R) --> View Replay", 950, 450);
+		con.repaint();
+		
+		intKey = con.getChar();
+		
+		
 	}
 }
